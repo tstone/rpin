15c77692bb407fcb7aaa7d5f1e0f1189ee92d03e
tstone <wastingtape@gmail.com>
2025-03-15 16:59:52 -0600

    only enable dev tools when debug

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,18 @@ mod fast_pinball;
 mod pinball;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(LogPlugin {
-            filter: "info,wgpu_core=warn,wgpu_hal=warn,bevy_pin::pinball=debug".to_string(),
-            level: Level::DEBUG,
-            ..Default::default()
-        }))
-        .add_systems(Startup, spawn_fake_cabinet_hardware)
-        .add_plugins(PinballBase)
-        .add_plugins(PaymentPlugin {
-            required_credits: 2,
-            ..Default::default()
-        })
-        .add_plugins(PinballDebugLogger)
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins.set(LogPlugin {
+        filter: "info,wgpu_core=warn,wgpu_hal=warn,bevy_pin::pinball=debug".to_string(),
+        level: Level::DEBUG,
+        ..Default::default()
+    }))
+    .add_systems(Startup, spawn_fake_cabinet_hardware)
+    .add_plugins(PinballBase)
+    .add_plugins(PaymentPlugin::default());
+
+    #[cfg(debug_assertions)]
+    app.add_plugins(PinballDebugLogger)
         .add_plugins(SwitchEmulator(HashMap::from([(
             KeyCode::Enter,
             CabinetButtons::StartButton,
@@ -33,6 +32,7 @@ fn main() {
         .add_plugins(SwitchEmulator(HashMap::from([(
             KeyCode::Comma,
             CabinetSwitches::AddCoin,
-        )])))
-        .run();
+        )])));
+
+    app.run();
 }
